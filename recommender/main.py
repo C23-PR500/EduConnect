f97cb2d04f9474d07903fa64dc371eee6eb970e2
azzam.labib <azzam.labib@ui.ac.id>
2023-06-14 17:55:57 +0700

    Implemented Recommender API

---
+++ recommender/main.py
@@ -0,0 +1,48 @@
+'''
+user: string (name)
+
+jobs_list: dictionary[]
+[
+{
+            "name": "John Doe2",
+            "city": "Semarang",
+            "area": "Central Java",
+            "country": "Indonesia",
+            "skills": ["Teaching", "Mentoring"]
+},
+{
+
+}, ...
+]
+
+users_list: dictionary[]
+[
+{
+            "name": "Teacher",
+            "companyName": "KenzieTech International Corporation",
+            "level": "Associate",
+            "city": "Semarang",
+            "area": "Central Java",
+            "country": "Indonesia"
+            "skills": ["Teaching", "Mentoring"]
+} 
+,
+{
+
+},
+...
+]
+'''
+import pandas as pd
+from data_preprocesser import data_preprocesser
+from similarity_processer import predict_jobs_similarity
+
+# return ["Job 1;;;Company Name", "Job 2;;;Company Name"]
+def predict_job(user, jobs_list, users_list, n = 10):
+    jobs_df = pd.DataFrame(jobs_list)
+    users_df = pd.DataFrame(users_list)
+    users_df, jobs_df = data_preprocesser(users_df, jobs_df)
+    similar_jobs_df = predict_jobs_similarity(user, users_df, jobs_df, n)
+    similar_jobs_id_list = similar_jobs_df.index.tolist()
+    return similar_jobs_id_list
+
